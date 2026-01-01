eb4f113d1d3d73af926546726131065ba7ca2c44
Shailesh <shaileshutkar77@gmail.com>
2026-01-01 16:23:22 +0530

    di half

---
+++ SpringProgram9Autowiring/src/main/java/in/sp/resources/SpringConfigFile.java
@@ -0,0 +1,33 @@
+package in.sp.resources;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import in.sp.beans.Address;
+import in.sp.beans.Student;
+
+@Configuration
+public class SpringConfigFile {
+
+	@Bean
+	public Address returnAddrObj() {
+		Address addr = new Address();
+
+		addr.setHouseNo(100);
+		addr.setCity("Pune");
+		addr.setPincode(123456);
+
+		return addr;
+	}
+
+	@Bean
+	public Student returnStdObj() {
+		Student std = new Student();
+
+		std.setName("Shailesh");
+		std.setRollNo(101);
+		std.setAddress(returnAddrObj());
+
+		return std;
+	}
+}
